f15a6c63fe42cc1bbb50678e6c51d3e049b856f0
Errol Jade <139873979+EJcarcedo@users.noreply.github.com>
2025-05-06 16:36:59 +0800

    Update firebase.js
    You must fill-up your own API key

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGyDwIKn4p2R1e3_8K_2Yczbq4eUMc2bg",
-  authDomain: "heatcheck-51699.firebaseapp.com",
-  databaseURL: "https://heatcheck-51699-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "heatcheck-51699",
-  storageBucket: "heatcheck-51699.appspot.com",
-  messagingSenderId: "855545883466",
-  appId: "1:855545883466:web:84c026f6368609b8b32fb3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
   const app = initializeApp(firebaseConfig);
